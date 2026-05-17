3e80759083684cf9183404483b39f421e94b0b77
jkaminska2 <joanna.kam55@gmail.com>
2026-05-17 18:52:15 +0200

    Poprawa testu

--- src/test/java/javamarkt/BestPromotionOrderTest.java
+++ src/test/java/javamarkt/BestPromotionOrderTest.java
@@ -2,6 +2,7 @@ package javamarkt;
 
 import org.junit.jupiter.api.Test;
 import java.util.List;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,11 +17,11 @@ class BestPromotionOrderTest {
                 new Product("C", "C", 100)
         );
 
-        List<Promotion> promotions = List.of(
+        List<Promotion> promotions = new ArrayList<>(List.of(
                 new PromoOrderDiscount(200, 0.10),
                 new PromoBuy2Get1(),
                 new PromoCoupon("A", 0.30)
-        );
+        ));
 
         List<Product> best = BestPromotionOrder.applyInBestOrder(cart, promotions);
 
